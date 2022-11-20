e1ad57355897fb374a5a818543bd09a94bcc8087
abhiyankhanal <abhiyan5588>
2022-11-21 00:51:26 +0545

    create post

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"e/model"
 	"e/utilities"
 	"fmt"
+	"math/rand"
 
 	"net/http"
 	"os"
@@ -21,65 +22,84 @@ func main() {
 	}
 	r.POST("/login", db.login)
 	r.POST("/register", db.register)
+	r.POST("/:user/post", db.createPost)
 	r.GET("/home", home)
 
 	r.Run()
 }
 
-func (db dbManager) login(c *gin.Context) {
+func (db dbManager) login(ctx *gin.Context) {
 	var user model.User
 	var token model.Token
 
-	c.BindJSON(&user)
+	ctx.BindJSON(&user)
 
 	client, err := db.connect(db.URL)
 	if err != nil {
 		fmt.Print(err)
 	}
 	collection := client.Database("test").Collection("users")
-
 	filter := bson.M{"username": user.Username, "password": user.Password}
-
 	err = collection.FindOne(context.Background(), filter).Decode(&user)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
 		return
 	}
 
 	token.Token = utilities.CreateToken(user.Username)
-	c.JSON(http.StatusOK, token)
+	ctx.JSON(http.StatusOK, token)
 }
 
-func (db dbManager) register(c *gin.Context) {
+func (db dbManager) register(ctx *gin.Context) {
 	var user model.User
-	c.BindJSON(&user)
+	ctx.BindJSON(&user)
 	client, err := db.connect(db.URL)
 	if err != nil {
 		fmt.Print(err)
 	}
 	collection := client.Database("test").Collection("users")
-
 	user.Token = utilities.CreateToken(user.Username)
+	//TODO validate if it is present in db
+	user.ID = fmt.Sprint(rand.Intn(9999999))
 	_, err = collection.InsertOne(context.Background(), user)
 	if err != nil {
-		c.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
+		ctx.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"data": "User created"})
+	ctx.JSON(http.StatusCreated, gin.H{"data": "User created"})
 }
 
-func home(c *gin.Context) {
-	token := c.Request.Header.Get("Authorization")
+func (db dbManager) createPost(ctx *gin.Context) {
+	var post model.Post
+	ctx.BindJSON(&post)
+	client, err := db.connect(db.URL)
+	if err != nil {
+		fmt.Print(err)
+	}
+	collection := client.Database("test").Collection("posts")
+	//TODO validate if it is present in db
+	post.ID = fmt.Sprint(rand.Int())
+	post.UserID = ctx.Param("user")
+	_, err = collection.InsertOne(context.Background(), post)
+	if err != nil {
+		ctx.JSON(http.StatusConflict, gin.H{"error": "Error while creating post"})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"data": "Post Created Successfully"})
+}
+
+func home(ctx *gin.Context) {
+	token := ctx.Request.Header.Get("Authorization")
 	token = token[7:len(token)]
 	claims := jwt.MapClaims{}
 	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("SECRET")), nil
 	})
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": "Home"})
+	ctx.JSON(http.StatusOK, gin.H{"data": "Home"})
 }
--- model/model.go
+++ model/model.go
@@ -4,7 +4,14 @@ type User struct {
 	ID       string `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Friends  []User `json:"friends"`
 	Token    string `json:"token"`
+}
+
+type Post struct {
+	ID     string `json:"id"`
+	Body   string `json:"body"`
+	UserID string `json:"user_id"`
 }
 
 type Token struct {
